aa9a96f152c93e17346403997e414a061f3e1d53
Marius Mandal <mariusmandal@gmail.com>
2013-10-14 14:45:36 +0200

    smas dependencies again
    thank you cmd+z!

--- include/sms/newphone.inc.php
+++ include/sms/newphone.inc.php
@@ -1,5 +1,5 @@
 <?php
-	require_once('include/api.smas.inc.php');
+//	require_once('include/api.smas.inc.php');
 	require_once('include/password.inc.php');
 	###########################################################
 	###########################################################
