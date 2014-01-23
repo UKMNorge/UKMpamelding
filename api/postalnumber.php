77581391f2346890e74af8d5b8c019e66df784fc
jardarn <jardar@ukm.no>
2014-01-23 11:45:03 +0100

    La til session_save_path og session.gc_probability

--- api/postalnumber.php
+++ api/postalnumber.php
@@ -1,6 +1,8 @@
 <?php
 ini_set('session.cookie_domain', 'pamelding.ukm.no');
 ini_set('session.gc_maxlifetime', 60*60*8);
+session_save_path('/tmp/pamelding.ukm.no');
+ini_set('session.gc_probability', 1);
 session_start();
 require_once('../include/database.inc.php');
 $query = new SQL("SELECT `postalplace` FROM `smartcore_postalplace` WHERE `postalcode` = '#code'",
